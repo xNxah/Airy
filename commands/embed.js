de9b358a3ad544eb7828c10b1644dbecfc8eda2b
xNxah <noah.dueholm.borg@gmail.com>
2021-09-01 17:34:51 +0200

    v3 fix2.3

--- commands/embed.js
+++ commands/embed.js
@@ -7,7 +7,7 @@ module.exports = {
     execute(client, message, args){
 let title = args[0]; 
   let image = args[1];
-  let desc = args.slice(2).join(" "); // try args.join(" ") - run console.log(desc) 
+  let desc = args.slice(1).join(" "); // try args.join(" ") - run console.log(desc) 
  if(!args[1]) return message.reply('no argument');
  if(args[3]) return message.reply('Too many arguments')
  const Embed =  new MessageEmbed()
