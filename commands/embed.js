8f066a3278edd049bd739487bb0d466c5ead2e5e
xNxah <noah.dueholm.borg@gmail.com>
2021-09-01 17:07:29 +0200

    v3

--- commands/embed.js
+++ commands/embed.js
@@ -2,23 +2,19 @@ const {MessageEmbed} = require('discord.js')
 
 module.exports = {
     name: 'embed',
-    description: "Custom embed",
-    permissions: ["READ_MESSAGE_HISTORY"],
+    description: "embed",
+    permissions: ["ADMINISTRATOR", "BAN_MEMBERS"],
     execute(client, message, args){
-
-        const Embed = new MessageEmbed()
-        .setTitle('Roles')
-        .setColor('#FFFFFF')
-        .setDescription(`To make pings minimal, we have designed a system allowing you to pick which notifications you would like to recieve. 
-
-Announcements = :newspaper2: 
-Development = :tools: 
-Giveaways = :tada: 
-Events = :medal: 
-
-To assign any of the roles, please react below this message with the reaction matching the notification. `)
-        .setImage('https://media.discordapp.net/attachments/881907696268374026/881910346879741952/airesRoles.jpg?width=1440&height=360')
-
-        message.channel.send({ embeds: [Embed]});
-    }
-}
+let title = args[0]; 
+  let desc = args.slice(1).join(" "); // try args.join(" ") - run console.log(desc) 
+  let image = args[2];
+ if(!args[1]) return message.reply('no argument');
+ if(args[3]) return message.reply('Too many arguments')
+ const Embed =  new MessageEmbed()
+    .setColor("#ff1233")
+    .setTitle(title)
+    .setImage(image)
+    .setDescription(desc);
+message.reply('Sending now.. :point_right:'
+   message.channel.send({ embeds: [Embed]});
+ 
