ce10eb778bfa47caa7bc943cb3b0ccf87a0ce6e7
michael <72696420+michaelrbx@users.noreply.github.com>
2021-09-01 20:47:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,11 @@ const fs = require('fs');
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
-
+  client.on("ready", () => {
+client.user.setActivity(`AIRES | -help`, { type: 'WATCHING' })
+        .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
+        .catch(console.error);
+    });
 
 ['command_handler', 'event_handler'].forEach(handler => {
     require(`./handlers/${handler}`)(client, Discord);
