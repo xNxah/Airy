6633ed973b0c7901eaacf1982074a6bab153fa4f
xNxah <noah.dueholm.borg@gmail.com>
2021-09-01 18:27:14 +0200

    v4

--- commands/embed.js
+++ commands/embed.js
@@ -4,16 +4,18 @@ module.exports = {
     name: 'embed',
     description: "embed",
     permissions: ["ADMINISTRATOR", "BAN_MEMBERS"],
-    execute(client, message, args){
-let title = args[0]; 
-  let image = args[1];
-  let desc = args.slice(2).join(" "); // try args.join(" ") - run console.log(desc) 
- if(!args[1]) return message.reply('no argument');
- const Embed =  new MessageEmbed()
-    .setColor("#ff1233")
-    .setTitle(title)
-    .setImage(image)
-    .setDescription(desc);
+    execute(client, message, args){ 
+        let channel = args[1]
+        let color = args[2];
+        let title = args[3]; 
+        let image = args[4];
+        let desc = args.slice(5).join(" ");
+        if(!args[1]) return message.reply('no argument');
+        const Embed =  new MessageEmbed()
+        .setColor(color)
+        .setTitle(title)
+        .setImage(image)
+        .setDescription(desc);
 message.reply('Sending now.. :point_right:')
 message.channel.send({ embeds: [Embed]})
 }}
