9d2e5c775987cb85da0873185e8a6c92c052042c
xNxah <noah.dueholm.borg@gmail.com>
2021-09-01 21:13:01 +0200

    membercount + noblox

---
+++ commands/membercount.js
@@ -0,0 +1,17 @@
+const {MessageEmbed, Guild} = require('discord.js')
+
+module.exports = {
+    name: 'membercount',
+    description: "Show's how many members are in the server. ",
+    permissions: ["READ_MESSAGE_HISTORY"],
+    execute(client, message, args){
+
+        var userCount = Guild.memberCount;
+
+        const Embed = new MessageEmbed()
+        .setColor('#11064b')
+        .setDescription(userCount)
+
+        message.reply({ embeds: [Embed]})
+    }
+}
