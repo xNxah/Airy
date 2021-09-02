14a0afc507f0c93da5a1b5e32967548e4fc8f2d5
xNxah <noah.dueholm.borg@gmail.com>
2021-09-02 16:55:12 +0200

    demotefix1

--- commands/demote.js
+++ commands/demote.js
@@ -2,15 +2,15 @@ const noblox = require('noblox.js');
 const {MessageEmbed} = require('discord.js')
 const groupID = '11938979'
 module.exports = {
-    name: 'demote',
-    description: "demotes (obviously)",
-    permissions: ["READ_MESSAGE_HISTORY"], 
-    execute(client, message, args){ 
-      const robloxname = args[0]
-      if (!robloxname) return  message.reply("Please run the command again and provide a username.")
-      const robloxid = noblox.getIdFromUsername(robloxname)
-      const oldRnk = noblox.getRankNameInGroup(groupID, robloxid)
-                      noblox.demote(groupID, robloxid)
+  name: 'demote',
+  description: "demotes (obviously)",
+  permissions: ["READ_MESSAGE_HISTORY"], 
+  execute(client, message, args){ 
+    const robloxname = args[0]
+    if (!robloxname) return  message.reply("Please run the command again and provide a username.")
+    const robloxid = noblox.getIdFromUsername(robloxname)
+    const oldRank = noblox.getRankNameInGroup(groupID, robloxid)
+    noblox.demote(groupID, robloxid)
                           .then(async (success) => {
 
                               const rankname = noblox.getRankNameInGroup(groupID, robloxid)
@@ -19,7 +19,7 @@ module.exports = {
                                   .setTitle('User Demoted')
                                   .setDescription(`**${robloxname}** was Demoted to "**${rankname}**"`)
                                   .addField('Group', (noblox.getGroup(groupID)).name)
-                                  .addField('Old Rank', oldRnk)
+                                  .addField('Old Rank', oldRank)
                                   .addField('Ranking User', user)
                                   .setURL(`https://www.roblox.com/users/${robloxid}/profile`)
                                   .setThumbnail(`https://www.roblox.com/headshot-thumbnail/image?userId=${robloxid}&width=420&height=420&format=png`)
