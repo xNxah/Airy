3466e74c1ba2e6ed38000a164570eed219c34ad6
xNxah <noah.dueholm.borg@gmail.com>
2021-09-02 17:50:07 +0200

    messageFix

--- commands/lookup.js
+++ commands/lookup.js
@@ -8,7 +8,7 @@ module.exports = {
     execute(client, message, args){ 
  let person = args[0]; 
   noblox.getIdFromUsername(person).catch(err => {
-            return msg.reply(
+            return message.reply(
               "sorry, that user doesn't seem to exist on ROBLOX."
             );
           });
