cdcb8ab4096b2bfe0845f2eb6b6e70b5801a7ce0
xNxah <noah.dueholm.borg@gmail.com>
2021-09-02 14:55:20 +0200

    8ball

--- commands/8ball.js
+++ commands/8ball.js
@@ -6,7 +6,7 @@ module.exports = {
     permissions: ["READ_MESSAGE_HISTORY"],
     execute(client, message, args){
 
-        const messages = ["Yes", "No", "Probably", "Maybe", "I don't know", "Lmao what no lol", "Wdym?", "Makes no sense for me-", "Bro leave me alone I'm tired", "zZzZzZzz oh what did you say?", "I can't tell right now"]
+        const messages = ["Yes", "No", "Probably", "Maybe", "I don't know", "Lmao what no lol", "Repeat?", "Makes no sense for me-", "zZzZzZzz oh what did you say?", "I can't tell right now"]
 
         const randomMessage = messages[Math.floor(Math.random() * messages.length)];
 
