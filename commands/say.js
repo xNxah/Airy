967c3f54c7d27d70ec27f25bee66b468c24c9dd2
xNxah <85689427+xNxah@users.noreply.github.com>
2021-09-06 11:00:18 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "Says whatever you'd like it to say. ",
     permissions: ["READ_MESSAGE_HISTORY"],
     execute(message, args){ 
-        let msg args.slice(1).join(` `);
+        let msg = args.slice(1).join(` `);
 		message.delete();
 		message.channel.send(msg);
 }}
