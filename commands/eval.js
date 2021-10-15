773af92f5d16f351081cfe7052455c65df1c3814
xNxah <noah.dueholm.borg@gmail.com>
2021-10-15 22:01:03 +0200

    uh eval lol

---
+++ commands/eval.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'eval',
+    description: "eval idk",
+    permissions: ["READ_MESSAGE_HISTORY"],
+    execute(client, message, args){ 
+
+    const args = message.content.split(" ").slice(1);
+    var ownerID = ["543447797270052864", "827986018027307060"]
+        if (message.author.id !== ownerID)
+        return;
+        try {
+        const evaled = eval(args.join(" "));
+        const cleaned = await clean(evaled);
+        message.channel.send(`\`\`\`js\n${cleaned}\n\`\`\``);
+      } catch (err) {
+        message.channel.send(`\`ERROR\` \`\`\`xl\n${cleaned}\n\`\`\``);
+      }
+    }
+}
