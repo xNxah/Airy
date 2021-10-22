f6b56ddea57ade1402a64ebdca7344ded752dc73
xNxah <noah.dueholm.borg@gmail.com>
2021-10-22 22:08:40 +0200

    portnew frick

--- commands/productembed.js
+++ commands/productembed.js
@@ -8,7 +8,7 @@ module.exports = {
         const embed =  new MessageEmbed()
         .setColor('11064b')
         .setTitle('Hunters Quay Port')
-        .setDescription(`RESELL FROM EUROHUB
+        .setDescription(`**RESELL FROM EUROHUB**
         
 An amazing island port originally made by euroHUB, and edited by airesTech. `)
         .setImage('https://media.discordapp.net/attachments/883971050935820298/901199408534806558/unknown.png?width=1306&height=662')
